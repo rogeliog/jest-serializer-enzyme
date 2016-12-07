e13836b87373155204fa80d375015f9eb47eb64a
Rogelio Guzman <rogelio.guzman@docker.com>
2016-12-07 09:19:51 -0800

    Make serializer work with components that render null

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ module.exports = {
   test(val) {
     return val.constructor && val.constructor.name === ShallowWrapper.name;
   },
-  print(val, serialize, indent) {
+  print(val) {
+    if (val.debug() === '') {
+      return 'null';
+    }
+
     return val.debug();
   }
 }
