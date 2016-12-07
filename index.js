33085f51d37e599ab14f19b587e6ccdef7489409
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-07 13:59:23 -0500

    added mount support

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var ShallowWrapper = require('enzyme/ShallowWrapper');
+var ReactWrapper = require("enzyme/ReactWrapper");
 
 module.exports = {
   test(val) {
-    return val.constructor && val.constructor.name === ShallowWrapper.name;
+    return (val.constructor && (
+      (val.constructor.name === ShallowWrapper.name)
+      || (val.constructor.name === ReactWrapper.name)
+    );
   },
   print(val, serialize, indent) {
     return val.debug();
