760e3cb483a1b69cb3d45a374b82c19e5f36760e
Jake Dawkins <dawkinsjh@gmail.com>
2016-12-07 14:18:08 -0500

    added check for existence of val and separated conditional

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ var ReactWrapper = require("enzyme/ReactWrapper");
 
 module.exports = {
   test(val) {
-    // check for val.constructor to exist and if val.constructor.name
-    // equals either the shallow wrapper name OR the react wrapper (mount) name.
-    return (
-      val.constructor && (
-        (val.constructor.name === ShallowWrapper.name)
-        || (val.constructor.name === ReactWrapper.name)
-      )
-    );
+    if (!val || !val.constructor) return false;
+
+    const doesMatch = (val.constructor.name === ShallowWrapper.name)
+      || (val.constructor.name === ReactWrapper.name);
+
+    return doesMatch;
   },
   print(val, serialize, indent) {
     return val.debug();
