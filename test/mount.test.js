d4dc603045fa0d668c0961339d36f587ef17ab80
w1nston <wallander.jonas@gmail.com>
2017-09-27 22:01:35 +0200

    Fix #15: Update enzyme-to-json dependency to v3.

--- test/mount.test.js
+++ test/mount.test.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import serializer from '../';
 
-const Bar = ({ text }) => <h1>{text}</h1>
+const adapter = new Adapter();
+
+const Bar = ({ text }) => <h1>{text}</h1>;
 
 const Foo = () => (
   <div>
     <span>Hello</span>
     <Bar text="some text" />
   </div>
-)
+);
 
 it('Serializes components', () => {
+  Enzyme.configure({ adapter });
   const wrapper = mount(<Foo />);
-  expect(wrapper).toMatchSnapshot()
+  expect(wrapper).toMatchSnapshot();
 });
 
 it('Serializes components when using find()', () => {
+  Enzyme.configure({ adapter });
   const wrapper = mount(<Foo />);
-  expect(wrapper.find('span')).toMatchSnapshot()
-  expect(wrapper.find('Bar')).toMatchSnapshot()
+  expect(wrapper.find('span')).toMatchSnapshot();
+  expect(wrapper.find('Bar')).toMatchSnapshot();
 });
 
 it('Serializes components that render null', () => {
-  const NullComponent = () => null
+  Enzyme.configure({ adapter });
+  const NullComponent = () => null;
   const wrapper = mount(<NullComponent />);
-  expect(wrapper).toMatchSnapshot()
+  expect(wrapper).toMatchSnapshot();
 });
--- test/shallow.test.js
+++ test/shallow.test.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import serializer from '../';
 
-const Bar = ({ text }) => <h1>{text}</h1>
+const adapter = new Adapter();
+
+const Bar = ({ text }) => <h1>{text}</h1>;
 
 const Foo = () => (
   <div>
     <span>Hello</span>
     <Bar text="some text" />
   </div>
-)
+);
 
 it('Serializes components', () => {
+  Enzyme.configure({ adapter });
   const wrapper = shallow(<Foo />);
-  expect(wrapper).toMatchSnapshot()
+  expect(wrapper).toMatchSnapshot();
 });
 
 it('Serializes components when using find() and dive()', () => {
+  Enzyme.configure({ adapter });
   const wrapper = shallow(<Foo />);
-  expect(wrapper.find('span')).toMatchSnapshot()
-  expect(wrapper.find('Bar').dive()).toMatchSnapshot()
+  expect(wrapper.find('span')).toMatchSnapshot();
+  expect(wrapper.find('Bar').dive()).toMatchSnapshot();
 });
 
 it('Serializes components that render null', () => {
-  const NullComponent = () => null
+  Enzyme.configure({ adapter });
+  const NullComponent = () => null;
   const wrapper = shallow(<NullComponent />);
-  expect(wrapper).toMatchSnapshot()
+  expect(wrapper).toMatchSnapshot();
 });
